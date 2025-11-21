c51a998e4d235ec7052344290cba151353daae90
yonggyo1125 <yonggyo00@kakao.com>
2025-11-21 11:05:48 +0900

    ...

--- src/main/java/org/spartahub/gatewayserver/GatewayServerApplication.java
+++ src/main/java/org/spartahub/gatewayserver/GatewayServerApplication.java
@@ -9,5 +9,4 @@ public class GatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/org/spartahub/gatewayserver/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package org.spartahub.gatewayserver.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi userAPI() {
+        return GroupedOpenApi.builder()
+                .group("user-api")
+                .displayName("USER API")
+                .pathsToMatch("/v1/user/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi hubAPI() {
+        return GroupedOpenApi.builder()
+                .group("hub-api")
+                .displayName("HUB API")
+                .pathsToMatch("/v1/hub/**")
+                .build();
+    }
+}
